d03d92385efa508e1adadff9f563862ad3574d16
angel-luis <angel.luis@me.com>
2024-08-26 13:57:03 +0200

    Add id to dataset to avoid bugs

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -1,5 +1,5 @@
 import { Game } from "../types.definition";
 
 export default function CardList({ filteredData }: { filteredData: Game[] }) {
-  return filteredData.map(data => <p key={data.title}>{data.title}</p>);
+  return filteredData.map(data => <p key={data.id}>{data.title}</p>);
 }
--- src/types.definition.ts
+++ src/types.definition.ts
@@ -1,4 +1,5 @@
 export type Game = {
+  id: string;
   title: string;
   meta_critic_score: string;
   release_year: string;
